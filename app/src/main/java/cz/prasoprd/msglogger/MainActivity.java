672645228e1ee691fa8c553a91fb7251b1a3b943
prasoprd <71968268+prasoprd@users.noreply.github.com>
2021-01-30 12:10:13 +0100

    code optimization

--- app/src/main/java/cz/prasoprd/msglogger/MainActivity.java
+++ app/src/main/java/cz/prasoprd/msglogger/MainActivity.java
@@ -1,9 +1,7 @@
 package cz.prasoprd.msglogger;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -40,71 +38,57 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (api != null) {
-                    Snackbar.make(v,"Already logged in as " + api.getYourself().getDiscriminatedName(), Snackbar.LENGTH_LONG).show();
-                }
-                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("Enter your Discord token:");
-                EditText editText = new EditText(MainActivity.this);
-                editText.setInputType(InputType.TYPE_CLASS_TEXT);
-                dialog.setView(editText);
-                dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String text = editText.getText().toString();
-                        if (!text.isEmpty()) {
-                            token.setText(editText.getText().toString());
-                            Messages.createNotification(token.getText(),"Token");
-                        }
-                    }
-                });
-                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                dialog.show();
+        button.setOnClickListener(v -> {
+            if (api != null) {
+                Snackbar.make(v,"Already logged in as " + api.getYourself().getDiscriminatedName(), Snackbar.LENGTH_LONG).show();
             }
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle("Enter your Discord token:");
+            EditText editText = new EditText(MainActivity.this);
+            editText.setInputType(InputType.TYPE_CLASS_TEXT);
+            dialog.setView(editText);
+            dialog.setPositiveButton("Ok", (dialog1, which) -> {
+                String text = editText.getText().toString();
+                if (!text.isEmpty()) {
+                    token.setText(editText.getText().toString());
+                    Messages.createNotification(token.getText(),"Token");
+                }
+            });
+            dialog.setNegativeButton("Cancel", (dialog12, which) -> dialog12.cancel());
+            dialog.show();
         });
         if (api != null) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    api.addMessageDeleteListener(event -> {
-                        if (event.getMessage().isPresent() && event.getMessageAuthor().isPresent() && !event.getMessageAuthor().get().isYourself() && event.getMessageContent().isPresent() && !event.getMessageContent().get().isEmpty()) {
-                            if (event.getMessage().get().isServerMessage() && event.getServer().isPresent() && event.getServerTextChannel().isPresent()) {
-                                String text = event.getMessageContent().get();
-                                String name = event.getMessageAuthor().get().getDiscriminatedName();
-                                String channel = event.getServerTextChannel().get().getName();
-                                String server = event.getServer().get().getName();
-                                Messages.addDelete("\"" + text + "\" by " + name + " in " + channel + " from " + server);
-                            } else if (event.getMessage().get().isPrivateMessage()) {
-                                String text = event.getMessageContent().get();
-                                String name = event.getMessageAuthor().get().getDiscriminatedName();
-                                Messages.addDelete("\"" + text + "\" by " + name);
-                            }
+            new Thread(() -> {
+                api.addMessageDeleteListener(event -> {
+                    if (event.getMessage().isPresent() && event.getMessageAuthor().isPresent() && !event.getMessageAuthor().get().isYourself() && event.getMessageContent().isPresent() && !event.getMessageContent().get().isEmpty()) {
+                        if (event.getMessage().get().isServerMessage() && event.getServer().isPresent() && event.getServerTextChannel().isPresent()) {
+                            String text = event.getMessageContent().get();
+                            String name = event.getMessageAuthor().get().getDiscriminatedName();
+                            String channel = event.getServerTextChannel().get().getName();
+                            String server = event.getServer().get().getName();
+                            Messages.addDelete("\"" + text + "\" by " + name + " in " + channel + " from " + server);
+                        } else if (event.getMessage().get().isPrivateMessage()) {
+                            String text = event.getMessageContent().get();
+                            String name = event.getMessageAuthor().get().getDiscriminatedName();
+                            Messages.addDelete("\"" + text + "\" by " + name);
                         }
-                    });
-                    api.addMessageEditListener(event -> {
-                        if (event.getMessage().isPresent() && event.getMessageAuthor().isPresent() && !event.getMessageAuthor().get().isYourself() && event.getOldContent().isPresent() && !event.getOldContent().get().isEmpty()) {
-                            if (event.getMessage().get().isServerMessage() && event.getServer().isPresent() && event.getServerTextChannel().isPresent()) {
-                                String text = event.getOldContent().get();
-                                String name = event.getMessageAuthor().get().getDiscriminatedName();
-                                String channel = event.getServerTextChannel().get().getName();
-                                String server = event.getServer().get().getName();
-                                Messages.addEdit("\"" + text + "\" by " + name + " in " + channel + " from " + server);
-                            } else if (event.getMessage().get().isPrivateMessage()) {
-                                String text = event.getOldContent().get();
-                                String name = event.getMessageAuthor().get().getDiscriminatedName();
-                                Messages.addEdit("\"" + text + "\" by " + name);
-                            }
+                    }
+                });
+                api.addMessageEditListener(event -> {
+                    if (event.getMessage().isPresent() && event.getMessageAuthor().isPresent() && !event.getMessageAuthor().get().isYourself() && event.getOldContent().isPresent() && !event.getOldContent().get().isEmpty()) {
+                        if (event.getMessage().get().isServerMessage() && event.getServer().isPresent() && event.getServerTextChannel().isPresent()) {
+                            String text = event.getOldContent().get();
+                            String name = event.getMessageAuthor().get().getDiscriminatedName();
+                            String channel = event.getServerTextChannel().get().getName();
+                            String server = event.getServer().get().getName();
+                            Messages.addEdit("\"" + text + "\" by " + name + " in " + channel + " from " + server);
+                        } else if (event.getMessage().get().isPrivateMessage()) {
+                            String text = event.getOldContent().get();
+                            String name = event.getMessageAuthor().get().getDiscriminatedName();
+                            Messages.addEdit("\"" + text + "\" by " + name);
                         }
-                    });
-                }
+                    }
+                });
             }).start();
         }
         clazz = this;
